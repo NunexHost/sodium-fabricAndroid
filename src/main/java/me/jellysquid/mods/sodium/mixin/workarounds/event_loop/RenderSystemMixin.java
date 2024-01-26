ee7a1b18d6d6aa93e30f3cef20970f67df363268
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-01-26 00:28:54 -0300

    Update RenderSystemMixin.java

--- src/main/java/me/jellysquid/mods/sodium/mixin/workarounds/event_loop/RenderSystemMixin.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/workarounds/event_loop/RenderSystemMixin.java
@@ -1,9 +1,8 @@
 package me.jellysquid.mods.sodium.mixin.workarounds.event_loop;
 
 import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.render.Tessellator;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.Overwrite;
 
 @Mixin(RenderSystem.class)
@@ -11,10 +10,7 @@ public class RenderSystemMixin {
 
     @Overwrite(remap = false)
     public static void flipFrame(long window) {
-        pollEvents();
         RenderSystem.replayQueue();
         Tesselator.getInstance().getBuilder().clear();
-        pollEvents();
-
     }
 }
