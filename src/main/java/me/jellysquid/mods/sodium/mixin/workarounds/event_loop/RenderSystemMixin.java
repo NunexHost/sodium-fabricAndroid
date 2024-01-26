c0fd8ca14ff6086648311df011cbd1ecd9b90534
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-01-26 00:25:14 -0300

    Update RenderSystemMixin.java

--- src/main/java/me/jellysquid/mods/sodium/mixin/workarounds/event_loop/RenderSystemMixin.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/workarounds/event_loop/RenderSystemMixin.java
@@ -4,6 +4,7 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.Overwrite;
 
 @Mixin(RenderSystem.class)
 public class RenderSystemMixin {
