da858ecf0a1b562445ac66f8fc19630163e03612
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-01-26 13:28:56 -0300

    Update RenderSystemMixin.java

--- src/main/java/me/jellysquid/mods/sodium/mixin/workarounds/event_loop/RenderSystemMixin.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/workarounds/event_loop/RenderSystemMixin.java
@@ -1,29 +1,20 @@
 package me.jellysquid.mods.sodium.mixin.workarounds.event_loop;
 
 import com.mojang.blaze3d.systems.RenderSystem;
-import net.minecraft.client.render.Tessellator;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(RenderSystem.class)
 public class RenderSystemMixin {
-    
-    @Shadow private static void pollEvents() {}
-    
-    private static void removeFirstPoll() {
-        // noop
-        // should fix some bugs with minecraft polling events twice for some reason (why does it do that in the first place?)
-    }
+  @Redirect(method = "flipFrame", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;pollEvents()V", ordinal = 0))
+  private static void removeFirstPoll() {
+    // only skip poll if no events were received in the previous frame
+    if (!RenderSystem.getWindow().pollEvents()) {
+      return;
+    }
 
-    @Overwrite(remap = false)
-    public static void flipFrame(long window) {
-        // new code added here
-        pollEvents();
-        RenderSystem.replayQueue();
-        Tessellator.getInstance().getBuffer().clear();
-        pollEvents();
-    }
+    // noop
+    // should fix some bugs with minecraft polling events twice for some reason (why does it do that in the first place?)
+  }
 }
