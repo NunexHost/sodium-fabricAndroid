4c75f31a62c3327511c86e5925d7564e8976bb14
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-01-26 01:34:55 -0300

    Update RenderSystemMixin.java

--- src/main/java/me/jellysquid/mods/sodium/mixin/workarounds/event_loop/RenderSystemMixin.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/workarounds/event_loop/RenderSystemMixin.java
@@ -10,7 +10,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 
 @Mixin(RenderSystem.class)
 public class RenderSystemMixin {
-    @Redirect(method = "flipFrame", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;pollEvents()V", ordinal = 0))
+    
+    @Shadow private static void pollEvents() {}
+    
     private static void removeFirstPoll() {
         // noop
         // should fix some bugs with minecraft polling events twice for some reason (why does it do that in the first place?)
